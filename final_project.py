25f0858bae6d039a62a22fd3da6753bd0778ef79
atlas23 <44505881+atlas23@users.noreply.github.com>
2021-11-15 20:12:48 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,30 +1,76 @@
+import time
+import random
 class Element:
-    def __init__(self, element, hp, strength):
+    """
+    Represents an element that the player can choose from
+    
+    Attributes:
+    element(str):the name of the element
+    hp(int): health points
+    strength(int): power level
+    luck(int): amount of luck for the hero
+    """
+    def __init__(self, element, hp, strength, luck):
         self.element=element
         self.hp=hp
         self.strength=strength
+        self.luck=luck
         
-    def choose_player(self,ans):
-        print("\n Choose your player")
-        ans= input("").upper
-        if ans=="FIRE":
-            print("\n You have chosen fire. You have an advantage against grass")
+    def choose_player(self):
+        """
+        Chooses a player for the class.
+        
+        Args:
+        self(Element): an instantiated element.
+        
+        Raises:
+        KeyError: invalid input
+        
+        Side effects:
+        prints to console
+        """
+        print("\n Choose your Element: FIRE, WATER, or GRASS")
+        
+        ans= input().upper()
+        string= ""
+        if ans== "FIRE":
             self.element= "FIRE"
-            self.hp= 100
+            print(f"""\n You have chosen {self.element}.
+                       \n You have an advantage against GRASS.
+                        \n You have a disadvantage against WATER.\n """)
+            self.hp=100
             self.strength=10
-        elif ans== "ICE":
-            print("\n You have chosen ice. You have an advantage against fire")
-            self.element="ICE"
+        elif ans== "WATER":
+            self.element="WATER"
+            print(f"""\n You have chosen {self.element}.
+                       \n You have an advantage against FIRE.
+                        \n You have a disadvantage against GRASS.\n """)
             self.hp=100
             self.strength=10
         elif ans== "GRASS":
-            print("\n You have chosen grass. You have an advantages against water.")
             self.element="GRASS"
+            print(f"""\n You have chosen {self.element}.
+                       \n You have an advantage against WATER.
+                        \n You have a disadvantage against FIRE.\n """)
+            
             self.hp=100
             self.hp=10
         else:
             raise KeyError("Invalid Player." 
                            +" Please select from the following:"
                            +" FIRE, WATER, GRASS")
-
-    
+        x= input("Feeling lucky? Y/N to roll the dice.\n").upper()
+        if x=="N":
+            print("Playing it safe I see. ")
+        elif x=="Y":   
+            time.sleep(.3)
+            print("Let's see what you get! We roll the dice.\n ")
+            time.sleep(2)
+            print("Still rolling...\n")
+            time.sleep(2)
+            self.luck=random.randint(0,3)
+            print(f"Your player has {self.luck} luck out of 3 \n")
+        else:
+            raise KeyError("Invalid input. Type 'Y' for Yes, or 'N' for No.")
+        
+            
