86dd105ac4f73028365995f44763eb82ac9ccd4e
Anternuy <anthonysadegray@gmail.com>
2021-11-15 20:40:20 -0500

    Opponent class
    Opponent Class created along with csv file

---
+++ Opponent.py
@@ -0,0 +1,25 @@
+"""
+Class for Opponent
+"""
+
+class Opponent:
+    """
+    Enemies player faces when selecting rooms
+    
+    attributes:
+    filename(str) path to csv file containing opponent element, hp, and strength
+    """
+    
+    def __init__(self, element, hp, strength, luck):
+        self.element = element
+        self.hp = hp
+        self.strength = strength
+        self.luck = luck
+    
+    def attack_method(self, player):
+        """
+        method for opponent to attack player
+        
+        args:
+            player(Player object)
+        side effect;
